6aeec1ade3def4bfe9e78556d0b7065ecdfe5c62
Mark Szulc <mszulc@adobe.com>
2022-05-08 15:50:03 +1000

    Initial Commit

---
+++ Frescopa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Frescopa
+//
+//  Created by Mark Szulc on 8/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Frescopa/FrescopaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrescopaApp.swift
+//  Frescopa
+//
+//  Created by Mark Szulc on 8/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct FrescopaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
